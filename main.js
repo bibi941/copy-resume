48264cfb27ae39e742662dbc70f48cd3eeec6f53
finley <sssrrr@vip.qq.com>
2018-01-29 23:54:40 +0800

    selecTopbar

--- main.js
+++ main.js
@@ -8,7 +8,27 @@ portfolio3.onclick = function() {
   portfolioBar.className = 'bar state-3'
 }
 window.onscroll = function() {
+  //move-topbar
   window.scrollY > 0
     ? topNavbar.classList.add('sticky')
     : topNavbar.classList.remove('sticky')
+}
+let aTags = document.getElementsByClassName('menuTigger')
+for (let i = 0; i < aTags.length; i++) {
+  aTags[i].onmouseenter = function(x) {
+    let a=x.currentTarget
+    let brother=a.nextSibling
+    while (brother.nodeType===3) {
+      brother=brother.nextSibling
+    }
+    brother.classList.add('active')
+  }
+  aTags[i].onmouseleave = function(x) {
+    let a=x.currentTarget
+     let brother = a.nextSibling
+     while (brother.nodeType === 3) {
+       brother = brother.nextSibling
+     }
+     brother.classList.remove('active')
+  }
 }
