941ca3aa9e1331912d86a77ed0ded92c82b028e8
finley <sssrrr@vip.qq.com>
2018-01-30 17:51:36 +0800

    添加loading，添加nav动画，添加描点定位

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+setTimeout(function () {
+  siteWelcome.classList.remove('active')
+},1500)
+
 portfolio1.onclick = function() {
   portfolioBar.className = 'bar state-1'
 }
@@ -7,28 +11,36 @@ portfolio2.onclick = function() {
 portfolio3.onclick = function() {
   portfolioBar.className = 'bar state-3'
 }
+
+
 window.onscroll = function() {
   //move-topbar
   window.scrollY > 0
     ? topNavbar.classList.add('sticky')
     : topNavbar.classList.remove('sticky')
 }
-let aTags = document.getElementsByClassName('menuTigger')
-for (let i = 0; i < aTags.length; i++) {
-  aTags[i].onmouseenter = function(x) {
-    let a=x.currentTarget
-    let brother=a.nextSibling
-    while (brother.nodeType===3) {
-      brother=brother.nextSibling
-    }
-    brother.classList.add('active')
+
+
+let liTags = document.getElementsByClassName('menuTigger')
+for (let i = 0; i < liTags.length; i++) {
+  liTags[i].onmouseenter = function(x) {
+    let li = x.currentTarget.classList.add('active')
   }
-  aTags[i].onmouseleave = function(x) {
-    let a=x.currentTarget
-     let brother = a.nextSibling
-     while (brother.nodeType === 3) {
-       brother = brother.nextSibling
-     }
-     brother.classList.remove('active')
+  liTags[i].onmouseleave = function(x) {
+    let li = x.currentTarget.classList.remove('active')
+  }
+}
+
+
+let aTags = document.querySelectorAll('nav>ul>li>a')
+for (let i = 0; i < aTags.length; i++) {
+  aTags[i].onclick = function(x) {
+    //监听
+    x.preventDefault() //阻止默认事件
+    let a = x.currentTarget
+    let href = a.getAttribute('href')
+    let element = document.querySelector(href)
+    let top = element.offsetTop//距离页面顶部的像素
+    window.scrollTo(0,top-80)
   }
 }
